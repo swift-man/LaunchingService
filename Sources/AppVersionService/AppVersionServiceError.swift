43b81d3a7c67dfd48b24646db196964c282d2a95
swift-man <jiniopening@naver.com>
2023-02-05 23:45:16 +0900

    [Feat] Error 구체화

--- Sources/AppVersionService/AppVersionServiceError.swift
+++ Sources/AppVersionService/AppVersionServiceError.swift
@@ -8,11 +8,28 @@
 import Foundation
 
 // MARK: - Enums
-public enum AppVersionServiceError: Error {
+public enum AppVersionServiceError: Equatable, LocalizedError, Sendable {
   case invalidAppStoreURLString
   case notFoundAppStoreURLString
   case notFoundForceAppVersionKey
   case notFoundOptionalUpdateVersionKey
   case notFoundReleaseVersionNumber
   case unknown
+  
+  public var errorDescription: String? {
+    switch self {
+    case .invalidAppStoreURLString:
+      return "Invalid AppStoreURLString to nil."
+    case .notFoundAppStoreURLString:
+      return "Unknown user firebase remoteConfig Key (AppStore URL)"
+    case .notFoundForceAppVersionKey:
+      return "Unknown user firebase remoteConfig Key (Force App Version)"
+    case .notFoundOptionalUpdateVersionKey:
+      return "Unknown user firebase remoteConfig Key (Optional Update Version)"
+    case .notFoundReleaseVersionNumber:
+      return "Unknown user Bundle.main - Release Version Number"
+    case .unknown:
+      return "Unknown"
+    }
+  }
 }
